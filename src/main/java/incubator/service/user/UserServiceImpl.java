f727eae84a095f7ea0e3deed30f24dc4aa9707f1
arhbear <5472469@gmail.com>
2020-10-27 21:22:56 +0300

    Create UserService interface, create UserServiceImpl class

---
+++ src/main/java/incubator/service/user/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package incubator.service.user;
+
+import incubator.entity.User;
+import incubator.repository.UserRepos;
+import incubator.service.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepos userRepos;
+
+    public User getById(int id){ return userRepos.findById(id).get();}
+
+    @Override
+    public List<User> getAll() {
+        return userRepos.getAll();
+    }
+}
