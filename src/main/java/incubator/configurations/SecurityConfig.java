5bd7cf8c35a455276656e7ed0bb30cdb48fb792c
k-max1m <56435775+k-max1m@users.noreply.github.com>
2020-11-16 18:49:42 +0300

    Update
    Update

--- src/main/java/incubator/configurations/SecurityConfig.java
+++ src/main/java/incubator/configurations/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import sun.misc.CharacterEncoder;
 
 @Configuration
 @EnableWebSecurity
